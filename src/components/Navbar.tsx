769ba1e96b569f9d35b8a8c6c4cf8a812d4d0264
korostelevrostik-stack <korostelevrostik@gmail.com>
2026-09-19 20:50:23 +0500

    Add Navbar component with navigation links

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h2>IELTS Prep</h2>
+      </div>
+
+      <div>
+        <a href="#">Dashboard</a>
+        <a href="#">Listening</a>
+        <a href="#">Reading</a>
+        <a href="#">Writing</a>
+        <a href="#">Speaking</a>
+        <a href="#">Vocabulary</a>
+        <a href="#">Statistics</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
