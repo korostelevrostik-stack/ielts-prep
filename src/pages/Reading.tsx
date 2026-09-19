577f3031e4a4cd04643de8bb02bfa7c95a4bdd13
korostelevrostik-stack <korostelevrostik@gmail.com>
2026-09-19 20:45:49 +0500

    Create Reading component for IELTS practice

---
+++ src/pages/Reading.tsx
@@ -0,0 +1,18 @@
+function Reading() {
+  return (
+    <div>
+      <h1>Reading</h1>
+
+      <p>Practice reading texts and answering IELTS-style questions.</p>
+
+      <h2>Practice types</h2>
+
+      <p>Multiple Choice</p>
+      <p>True / False / Not Given</p>
+      <p>Matching Headings</p>
+      <p>Sentence Completion</p>
+    </div>
+  )
+}
+
+export default Reading
